2d255abd803543bdfa9f6dad95b2cec5e43563cd
Виктор Алтунин <altvik@yandex.ru>
2023-01-03 21:40:42 +0600

    y_clouds

--- infra_project/infra_project/settings.py
+++ infra_project/infra_project/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'k9gwcfq01xszx2(_4e#gc3u$rt=)y6ivx=$wt+5&-^ktk4dg4e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['altvik2503.ddns.net']
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'altvik2503.ddns.net',
+]
 
 
 # Application definition
